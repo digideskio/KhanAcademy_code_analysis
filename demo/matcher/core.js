ccecea863c49ae5da946bc66ccf59306ddca9db3
Alex Rodrigues <lemiant@gmail.com>
2014-06-11 00:34:12 -0400

    3.0.2 Added a test case :). Done.

--- demo/matcher/core.js
+++ demo/matcher/core.js
@@ -18,24 +18,63 @@
 	}
 
 	var customTests = [
-    {tests: [{filter: "!while()"}], message: "Bad boy! No while loops."},
-	{
-        tests: [
-            {filter: "var $x, $y;", 
-             funcs: [ function($x, $y){ return ($x.name.slice(-1) == 'x' && $y.name.slice(-1) == "y")}.toString() ] }
-        ],
-        message: "Your pong game should have variables for x and y to track the ball's location"
-    },
-	{tests: [{filter: "var draw = function(){}"}], message: "Your pong game should have a draw function"},
-	{tests: [{filter: "var draw = function(){ Bx += _; }"}], message: "You should increment Bx in your draw function"},
+    {
+        tests: [{filter: "var _ = [$$_]; !;var _ = [_, _, _, $$_]; "}],
+        isError: true, 
+        message: "You must have three items in your array"
+    }, {
+        tests: [{filter: "var _ = [$a, $b, $c];", 
+                 funcs: [function($a, $b, $c){
+                        var isString = function(s){ console.log(s); return s && s.length >= 2 && (s[0] == '"' || s[0] == "'") && s[0] == s.slice(-1) }
+                        return !isString($a.raw) || !isString($b.raw) || !isString($c.raw)
+                        return 1;
+                    }.toString()]
+                }],
+        isError: true, 
+        message: "Your favourite animals must be strings"
+    }, {
+        tests: [{filter: "var _ = [_, _, _, $$_];"}], 
+        message: "Create a variable, and set it to an array with your three of favourite animals"
+    },	{
+        tests: [{filter: "var $c = 0; !;$c = [$$_]" }],
+        message: "Declare another variable to use as a counter and set it to 0"
+    },	{
+        tests: [{filter: "var $a = [$$_]; var $c = 0; while(){ } !while($c < $a.length)"}], 
+        isError: true,
+        message: "You can use YourArray.length to get the length of your array"
+    },	{
+        tests: [{filter: "var $a = [$$_]; var $c = 0; while(){ } !while(){ $c++ }"}], 
+        isError: true,
+        message: "You need to increment the counter in the loop or it will run forever"
+    },	{
+        tests: [{filter: "var $a = [$$_]; var $c = 0; while($c < $a.length){ $c++ }"}], 
+        message: "Use the counter to loop through all the animals with a while loop"
+    },	{
+        tests: [{filter: "var $a = [$$_]; var $c = 0; while($c < $a.length){ fill($$_) text($a[$c], _, _, $$_)}"}], 
+        message: "In the while loop set the fill() color and use the 'text()' function to output text."
+    },	{
+        tests: [{filter: "while(){ if() }"}], 
+        isError: true,
+        message: "You shouldn't need if statements for this demo"
+    },	{
+        tests: [{
+            filter: "var $a = [$$_]; var $c = 0; while($c < $a.length){ fill($$_) text($a[$c], _, _+($counter*$space), $$_)}",
+            funcs: [function($space){ return parseFloat($space.raw) > 10; }.toString()]
+        }], 
+        message: "Make sure to space the words out enough vertically"
+    },	{
+        tests: [{filter: "var _ = [_, _, _, _, _]"}],
+        message: "Extend the array 5 animals, to see that everything still works."
+    }
 	]
 	
 	var matchers = new threadPool()
+    var customHinterOn = false;
 
     function display_results(results){
 			var hint = "";
 			for(var i in customTests){
-				if(!results[i]){
+				if((!customTests[i].isError && !results[i]) || (customTests[i].isError && results[i])){
 					hint = customTests[i].message;
 					break;
 				}
@@ -45,6 +84,7 @@
 	}
         
 	window.customHinter = function(){
+        //if(!customHinterOn) return "fail";
         var post_tests = function(thread){ thread.postMessage(JSON.stringify({code: code, tests: customTests.map( function(test_obj){ return test_obj.tests } )})) }
 		var code = ScratchpadUI.editor.text();
 		var thread = matchers.getThread();
